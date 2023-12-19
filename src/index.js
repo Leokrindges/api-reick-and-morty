7c204b2b7348ef716f9e2c4224648646b4a8de6c
leonardo Krindges <krindgesleonardo3@gmail.com>
2023-12-18 23:40:08 -0300

    adiconado css na paginação e add rodapé, add readme

--- src/index.js
+++ src/index.js
@@ -9,6 +9,9 @@ let desconhecido
 let botaoPagina;
 let result = []
 
+const personagenEl = document.getElementById("personagen")
+const localizacaoEl = document.getElementById("localizacao")
+const episodioEl = document.getElementById("episodio")
 const cartoesPersonagensEL = document.getElementById("cartoes_personagens")
 const containerPersonagens = document.getElementById("personagens")
 const containerbotoes = document.getElementById("botoes")
@@ -20,10 +23,8 @@ const instance = axios.create({
 function aumentarPagina() {
     if (pagina !== quantidadeDePaginas) {
         pagina++
-
         containerbotoes.innerHTML = ''
         paginacao(pagina)
-
         carregarPersonagens()
     }
 }
@@ -32,59 +33,66 @@ function diminuirPagina() {
     if (pagina > 1) {
         pagina--
         containerbotoes.innerHTML = ''
-
         paginacao(pagina)
-
         carregarPersonagens()
     }
 }
 
-function selecionarPagina(novaPagina) {
-    pagina = novaPagina
-    paginacao(pagina)
-}
-
 function paginacao(novaPagina) {
     pagina = novaPagina
+    console.log(pagina);
 
+    //faz a paginação dos botões, para ficar mostrando a pagina anterior e posterior à página que esta selecionada
     if (pagina === quantidadeDePaginas - 1) {
         result = [1, quantidadeDePaginas - 3, quantidadeDePaginas - 2, pagina, quantidadeDePaginas]
-    } else if (pagina > 2 && pagina < quantidadeDePaginas) {
+    }
+    else if (pagina > 2 && pagina < quantidadeDePaginas) {
         result = [1, pagina - 1, pagina, pagina + 1, quantidadeDePaginas]
     }
     else if (pagina === quantidadeDePaginas) {
         result = [1, pagina - 3, pagina - 2, pagina - 1, quantidadeDePaginas]
-    } else {
+    }
+    else {
         result = [1, 2, 3, 4, quantidadeDePaginas]
     }
 
     containerbotoes.innerHTML = ''
 
+    // percore o array result e cria os novos botões usando os valores que estão setados dentro do array result
     for (let i = 0; i < result.length; i++) {
+
         botaoPagina = document.createElement('button')
+        botaoPagina.setAttribute('style', 'background-color: #62EC52; border:1px solid #62EC52; margin-right: 5px; width: 35px; height: 30px; border-radius: 5px; font-size:20px; margin-top: 40px;')
         botaoPagina.innerHTML = result[i]
         botaoPagina.addEventListener('click', () => { paginacao(result[i]) })
         containerbotoes.appendChild(botaoPagina)
-    }
 
+        //colocar cor no botão que esta selecionado
+        if (result[i] === pagina) {
+            botaoPagina.setAttribute('style', 'background-color: green; border:1px solid green; margin-right: 5px; width: 35px; height: 30px; border-radius: 5px; font-size:20px; margin-top: 40px;')
+        }
+    }
     carregarPersonagens()
 }
-
 
 async function carregamentoInicialPersonagens() {
     await carregarPersonagens()
     containerbotoes.innerHTML = ''
 
     for (let i = 0; i < quantidadeDePaginas; i++) {
+        //cria os botoes do 1 ao 4
         if (i < 4) {
             botaoPagina = document.createElement('button')
+            botaoPagina.setAttribute('style', 'background-color: #62EC52; border:1px solid #62EC52; margin-right: 5px; width: 35px; height: 30px; border-radius: 5px; font-size:20px; margin-top: 25px;')
             botaoPagina.innerHTML = i + 1
             botaoPagina.addEventListener('click', () => { paginacao(i + 1) })
             containerbotoes.appendChild(botaoPagina)
         }
 
-        if (i === 4) {
+        //seta a ultima posição do array result com a ultima pagina da api
+        if (i === 5) {
             botaoPagina = document.createElement('button')
+            botaoPagina.setAttribute('style', 'background-color: #62EC52; border:1px solid #62EC52; margin-right: 5px; width: 35px; height: 30px; border-radius: 5px; font-size:20px; margin-top: 40px;')
             botaoPagina.innerHTML = quantidadeDePaginas
             botaoPagina.addEventListener('click', () => { paginacao(quantidadeDePaginas) })
             containerbotoes.appendChild(botaoPagina)
@@ -93,6 +101,8 @@ async function carregamentoInicialPersonagens() {
 }
 
 async function carregarPersonagens() {
+    carregarLocalizações()
+    carregarEpisodios()
     try {
         const resposta = await instance.get(`/character?page=${pagina}`)
         const personagens = resposta.data.results
@@ -133,6 +143,26 @@ async function carregarPersonagens() {
             cartoesPersonagensEL.innerHTML += cartao
         });
 
+        personagenEl.innerHTML = `PERSONAGENS: ${count}`
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function carregarEpisodios() {
+    try {
+        const resposta = await instance.get(`/episode`)
+        episodioEl.innerHTML = `EPISÓDIOS: ${resposta.data.info.count}`
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function carregarLocalizações() {
+    try {
+        const resposta = await instance.get(`/location`)
+        localizacaoEl.innerHTML = `LOCALIZAÇÕES: ${resposta.data.info.count}`
     } catch (error) {
         console.log(error);
     }
